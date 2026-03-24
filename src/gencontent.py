01580f65c626802fe7999d36078214f075513eb0
adrake333 <adrake333@gmail.com>
2026-03-24 10:21:44 -0500

    added template.html and content/index.md

--- src/gencontent.py
+++ src/gencontent.py
@@ -5,3 +5,5 @@ def extract_title(markdown):
         if line.startswith("# "):
             return line[2:].strip()
     raise ValueError("invalid markdown - no h1")
+
+def generate_page(from_path, template_path, dest_path):
