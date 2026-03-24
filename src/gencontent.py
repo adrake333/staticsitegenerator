078fb0619227d111c521de8d148cf3eb25ec1f27
adrake333 <adrake333@gmail.com>
2026-03-24 11:50:41 -0500

    added generate_page

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
 import os
+from markdown_blocks import markdown_to_html_node
 
 def extract_title(markdown):
     for line in markdown.split("\n"):
@@ -7,3 +8,18 @@ def extract_title(markdown):
     raise ValueError("invalid markdown - no h1")
 
 def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        read_from = f.read()
+    with open(template_path) as f:
+        read_template = f.read()
+    html_node = markdown_to_html_node(read_from)
+    html_string = html_node.to_html()
+    title = extract_title(read_from)
+    new_content = read_template.replace("{{ Content }}", html_string)
+    new_template = new_content.replace("{{ Title }}", title)    
+    parent_dir = os.path.dirname(dest_path)
+    if parent_dir and not os.path.isdir(parent_dir):
+        os.makedirs(parent_dir)
+    with open(dest_path, "w") as f:
+        f.write(new_template)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from copystatic import copy_static
+from gencontent import generate_page
 
 def main():
     if os.path.exists("public"):
