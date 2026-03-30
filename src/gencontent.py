b4d251bec0b5e343ac00ed156485599640d59185
adrake333 <adrake333@gmail.com>
2026-03-30 11:45:58 -0500

    changed to generate at docs from public

--- src/gencontent.py
+++ src/gencontent.py
@@ -7,7 +7,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise ValueError("invalid markdown - no h1")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         read_from = f.read()
@@ -17,23 +17,24 @@ def generate_page(from_path, template_path, dest_path):
     html_string = html_node.to_html()
     title = extract_title(read_from)
     new_content = read_template.replace("{{ Content }}", html_string)
-    new_template = new_content.replace("{{ Title }}", title)    
+    new_template = new_content.replace("{{ Title }}", title)
+    new_template_2 = new_template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     parent_dir = os.path.dirname(dest_path)
     if parent_dir and not os.path.isdir(parent_dir):
         os.makedirs(parent_dir)
     with open(dest_path, "w") as f:
-        f.write(new_template)
+        f.write(new_template_2)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entry in os.listdir(dir_path_content):
         full_path = os.path.join(dir_path_content, entry)
         if os.path.isfile(full_path) and full_path.endswith(".md"):
             html_name = entry.replace(".md", ".html")
             dest_path = os.path.join(dest_dir_path, html_name)
-            generate_page(full_path, template_path, dest_path)
+            generate_page(full_path, template_path, dest_path, basepath)
         elif os.path.isdir(full_path):
             next_dest = os.path.join(dest_dir_path, entry)
             if not os.path.exists(next_dest):
                 os.mkdir(next_dest)
-            generate_pages_recursive(full_path, template_path, next_dest)
+            generate_pages_recursive(full_path, template_path, next_dest, basepath)
             
--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 import os
 import shutil
+import sys
 from copystatic import copy_static
 from gencontent import generate_pages_recursive
 
 def main():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
-    copy_static("static", "public")
-    generate_pages_recursive("content/", "template.html", "public/")
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
+    os.mkdir("docs")
+    copy_static("static", "docs")
+    generate_pages_recursive("content/", "template.html", "docs/", basepath)
 
 main()
